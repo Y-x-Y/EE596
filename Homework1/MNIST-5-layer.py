5433ebcffd30a762599a79258bfb1b918342b392
Xavier Yuan <xyuan@uw.edu>
2019-04-12 22:55:21 -0700

    update a deeper NN and write discussion in jupyther notebook.

--- Homework1/MNIST-5-layer.py
+++ Homework1/MNIST-5-layer.py
@@ -9,7 +9,7 @@ mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
 tf.logging.set_verbosity(old_v)
 
 #learning rate
-lr = 0.01
+lr = 0.001
 #number of traning epochs
 epochs = 20
 #number of batch_size
@@ -18,8 +18,11 @@ total_batch = int(mnist.train.num_examples/batch_size)
 num_steps = epochs * total_batch
 
 #network parameters
-n_hidden_1 = 200
+n_hidden_1 = 300
 n_hidden_2 = 200
+n_hidden_3 = 200
+n_hidden_4 = 60
+n_hidden_5 = 30
 num_input = 784
 num_classes = 10
 
@@ -32,12 +35,18 @@ Y = tf.placeholder(tf.int32,[None,num_classes],name='Y')
 weights = {
     'W1': tf.Variable(tf.random_normal([num_input, n_hidden_1],stddev=0.1),name='W1'),
     'W2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2],stddev=0.1),name='W2'),
-    'Wout': tf.Variable(tf.random_normal([n_hidden_2, num_classes],stddev=0.1),name='Wout')
+    'W3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3],stddev=0.1), name='W3'),
+    'W4': tf.Variable(tf.random_normal([n_hidden_3, n_hidden_4],stddev=0.1), name='W4'),
+    'W5': tf.Variable(tf.random_normal([n_hidden_4, n_hidden_5],stddev=0.1), name='W5'),
+    'Wout': tf.Variable(tf.random_normal([n_hidden_5, num_classes],stddev=0.1),name='Wout')
 }
 
 biases = {
     'b1': tf.Variable(tf.zeros(shape=[n_hidden_1]),name='b1'),
     'b2': tf.Variable(tf.zeros(shape=[n_hidden_2]),name='b2'),
+    'b3': tf.Variable(tf.zeros(shape=[n_hidden_3]),name='b3'),
+    'b4': tf.Variable(tf.zeros(shape=[n_hidden_4]),name='b4'),
+    'b5': tf.Variable(tf.zeros(shape=[n_hidden_5]), name='b5'),
     'bout': tf.Variable(tf.zeros(shape=[num_classes]),name='bout')
 }
 
@@ -45,7 +54,10 @@ biases = {
 def neural_net(x):
     layer_1_out = tf.nn.relu(tf.add(tf.matmul(x,weights['W1']),biases['b1']))
     layer_2_out = tf.nn.relu(tf.add(tf.matmul(layer_1_out,weights['W2']),biases['b2']))
-    out = tf.add(tf.matmul(layer_2_out,weights['Wout']),biases['bout'])
+    layer_3_out = tf.nn.relu(tf.add(tf.matmul(layer_2_out, weights['W3']), biases['b3']))
+    layer_4_out = tf.nn.relu(tf.add(tf.matmul(layer_3_out, weights['W4']), biases['b4']))
+    layer_5_out = tf.nn.relu(tf.add(tf.matmul(layer_4_out, weights['W5']), biases['b5']))
+    out = tf.add(tf.matmul(layer_5_out,weights['Wout']),biases['bout'])
     return out
 
 #predicted labels
